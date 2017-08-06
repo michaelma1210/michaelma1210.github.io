3efa74a32e987dc58adabaada982fbef3379f932
Michael Ma <michaelma1210@gmail.com>
2017-08-06 03:04:13 -0700

    added features

--- assets/js/inthefield.js
+++ assets/js/inthefield.js
@@ -52,4 +52,17 @@ $(document).ready(function(){
             $("#topnavbar").css("opacity", ".85");
         }
     });
+
+    document.onreadystatechange = function () {
+        var state = document.readyState
+        if (state == 'interactive') {
+            document.getElementById('contents').style.visibility="hidden";
+        } else if (state == 'complete') {
+            setTimeout(function(){
+                document.getElementById('interactive');
+                document.getElementById('load').style.visibility="hidden";
+                document.getElementById('contents').style.visibility="visible";
+            },1000);
+        }
+    }
 });
